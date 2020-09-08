42abcf230b710316d26d2fee5250ea56b8a4684b
Mariana G. Ferrarini <mari_ferrarini@hotmail.com>
2020-09-08 15:40:18 +0200

    Update EBSeq.r
    Corrected a small glitch on factors

--- scripts/Metabolic-fluxes/EBSeq.r
+++ scripts/Metabolic-fluxes/EBSeq.r
@@ -10,53 +10,53 @@ source("run_ebseq.R")
 count_table<-countData[,1:6]
 DE=as.factor(c("Mock","Mock","Mock","SARS-CoV-2","SARS-CoV-2","SARS-CoV-2"))
 outfile=paste("Series1.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("SARS-CoV-2", "Mock")),outfile)
 
 ### Series2
 count_table<-countData[,7:12]
 DE=as.factor(c("Mock","Mock","Mock","SARS-CoV-2","SARS-CoV-2","SARS-CoV-2"))
 outfile=paste("Series2.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("SARS-CoV-2", "Mock")),outfile)
 
 ### Series3
 count_table<-countData[,13:16]
 DE=as.factor(c("Mock","Mock","RSV","RSV"))
 outfile=paste("Series3.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("RSV", "Mock")),outfile)
 
 ### Series4
 count_table<-countData[,c(19,20,17,18)]
 DE=as.factor(c("Mock","Mock","IAV","IAV"))
 outfile=paste("Series4.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("IAV", "Mock")),outfile)
 
 ### Series5
 count_table<-countData[,21:26]
 DE=as.factor(c("Mock","Mock","Mock","SARS-CoV-2","SARS-CoV-2","SARS-CoV-2"))
 outfile=paste("Series5.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("SARS-CoV-2", "Mock")),outfile)
 
 ### Series7
 count_table<-countData[,27:32]
 DE=as.factor(c("Mock","Mock","Mock","SARS-CoV-2","SARS-CoV-2","SARS-CoV-2"))
 outfile=paste("Series7.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("SARS-CoV-2", "Mock")),outfile)
 
 ### Series8 HPIV3
 count_table<-countData[,c(36,37,38,33,34,35)]
 DE=as.factor(c("Mock","Mock","Mock","HPIV3","HPIV3","HPIV3"))
 outfile=paste("Series8H.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("HPIV3", "Mock")),outfile)
 
 ### Series8 RSV
 count_table<-countData[,36:41]
 DE=as.factor(c("Mock","Mock","Mock","RSV","RSV","RSV"))
 outfile=paste("Series8R.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("SARS-CoV-2", "Mock")),outfile)
 
 ### Series9 IAV
 count_table<-countData_entrez[,c(46:49,42:45)]
 DE=as.factor(c("Mock","Mock","Mock","Mock","IAV","IAV","IAV","IAV"))
 outfile=paste("Series9.csv",sep="")
-run_ebseq(count_table,DE,outfile)
+run_ebseq(count_table,factor(DE, levels=c("IAV", "Mock")),outfile)
 
